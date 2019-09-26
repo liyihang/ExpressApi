a2f35cc3f7b6e481031309423c78dc989c3cd936
liyihang <lidoudoou@gmail.com>
2019-09-26 18:51:23 +0800

    hot ranking page or index page

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,32 @@ let common = require('../common/common.json')
 /* 首页数据 追书热榜 top 20
 * 
  */
-router.get('/', function(req, res, next) {
-  
-  res.send(JSON.stringify({'code':2012301,'msg':'success123'}));
+router.get('/', function (req, res, next) {
+  request.get(`${common.API}/ranking/548e97e29fb698a01dc6ee6f`, function (err, response, body) {
+    if (err) {
+      res.json({
+        code: 400,
+        msg: 'load data failed'
+      })
+    }
+    body = JSON.parse(body);
+    if(body.ok){
+      let books = body.ranking.books.slice(0,19);
+      books.forEach(element => {
+        element.cover = common.API+element.cover;
+      });
+      res.json({
+        code:200,
+        books:books,
+        msg:'ok'
+      })
+    }else{
+      res.json({
+        code: 401,
+        msg: 'Token unvaliable'
+      })
+    }
+  });
 });
 
 module.exports = router;
